b4f3d6849dff9c10c6b1aa3f6b897be61250ad2d
NakanoMiku <jinleho@hotmail.com>
2023-11-30 03:18:06 +0800

    暂无变化

--- hello.s
+++ hello.s
@@ -1,12 +1,7 @@
   .text
   .globl main
 main:
-  li    t0, 6
-  xor    t0, t0, x0
-  seqz    t0, t0
-  li    t1, 1
-  sub    x0, x0, t1
-  li    t2, 1
-  sub    x0, x0, t1
+  li    t0, 1
+  li    a0, 1
   ret
 
